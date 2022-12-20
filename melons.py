fc040ab65c0f92a405ccf8388a94b362932f6ec6
June Balter <junedoeswork@gmail.com>
2022-12-20 10:23:46 -0600

    initial commit, added flesh/rind/weight dictionaries

---
+++ melons.py
@@ -0,0 +1,47 @@
+melon_names = {
+    1: 'Honeydew',
+    2: 'Crenshaw',
+    3: 'Crane',
+    4: 'Casaba',
+    5: 'Cantaloupe',
+}
+
+melon_prices = {
+    1: 0.99,
+    2: 2.00,
+    3: 2.50,
+    4: 2.50,
+    5: 0.99,
+}
+
+melon_seedlessness = {
+    1: True,
+    2: False,
+    3: False,
+    4: False,
+    5: False,
+}
+
+melon_flesh_color = {
+    1: None
+    2: None
+    3: None
+    4: None
+    5: None
+}
+
+melon_rind_color = {
+    1: None
+    2: None
+    3: None
+    4: None
+    5: None
+}
+
+melon_average_weight = {
+    1: None
+    2: None
+    3: None
+    4: None
+    5: None    
+}

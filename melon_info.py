bb8dbc5771445e5a4f25d0ae12517432fbf1d3ee
June Balter <junedoeswork@gmail.com>
2022-12-20 11:14:38 -0600

    with better solution

--- melon_info.py
+++ melon_info.py
@@ -3,20 +3,34 @@
 
 from melons import melons_register
 
-for melon in melons_register:
-
+def print_all_melons(melons_register):
     """Print each melon with corresponding attribute information."""
 
-    # have_or_have_not = 'have'
-    # if melons_register[melon]['seedless']:
-    #     have_or_have_not = 'do not have'
+    for melon_name, attributes in melons_register.items():
+        print(melon_name.upper())
 
-    print(f'{melon.upper()}:')
-    print(f'price:  ${melons_register[melon]["price"]}')
-    print(f'seedless: {melons_register[melon]["seedless"]}')
-    print(f'flesh color: {melons_register[melon]["flesh"]}')
-    print(f'rind color: {melons_register[melon]["rind"]}')
-    print(f'average weight: {melons_register[melon]["weight"]}\n')
+        for attribute, value in attributes.items():
+            print(f'{attribute}: {value}')
+
+        print('===================================')
+
+
+print_all_melons(melons_register)
+
+# for melon in melons_register:
+
+#     """Print each melon with corresponding attribute information."""
+
+#     # have_or_have_not = 'have'
+#     # if melons_register[melon]['seedless']:
+#     #     have_or_have_not = 'do not have'
+
+#     print(f'{melon.upper()}:')
+#     print(f'price:  ${melons_register[melon]["price"]}')
+#     print(f'seedless: {melons_register[melon]["seedless"]}')
+#     print(f'flesh color: {melons_register[melon]["flesh"]}')
+#     print(f'rind color: {melons_register[melon]["rind"]}')
+#     print(f'average weight: {melons_register[melon]["weight"]}\n')
 
 
 # for i in melon_names:
